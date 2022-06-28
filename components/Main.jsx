b2bd05a41c921f61273b05a561f7d363fec73f35
Mohamed Omar Moussa <77863041+Mohamed90-stack@users.noreply.github.com>
2022-06-28 10:34:22 +0200

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -9,16 +9,16 @@ const Main = () => {
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
                 <p className='uppercase text-sm tracking-widest text-gray-600'>
-                    LET'S BUILD SOMETHING TOGETHER
+                    LET&apos;S BUILD SOMETHING TOGETHER
                 </p>
                 <h1 className='py-4 text-gray-700'>
-                    Hi, I'm <span className='text-[#5651e5]'> Mohamed</span>
+                    Hi, I&apos;m <span className='text-[#5651e5]'> Mohamed</span>
                 </h1>
                 <h1 className='py-2 text-gray-700'>
                     A Front-End Web Developer
                 </h1>
                 <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
-                    I'm a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building responsive fullstack web applications while learning web3-blockchain technologies.
+                    I&apos;m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I&apos;m focused on building responsive fullstack web applications while learning web3-blockchain technologies.
                 </p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
