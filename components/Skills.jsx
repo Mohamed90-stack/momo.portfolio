497cc0c3056af51699d257ee1a85f490bc27b6dd
Mohamed Omar Moussa <77863041+Mohamed90-stack@users.noreply.github.com>
2023-09-15 11:09:06 +0200

    Update Skills.jsx

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -165,7 +165,7 @@ const Skills = () => {
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
                 <Image
-                  src='/assets/skills/shopify.png'
+                  src='/assets/skills/rails.png'
                   width='64px'
                   height='64px'
                   alt='/'
